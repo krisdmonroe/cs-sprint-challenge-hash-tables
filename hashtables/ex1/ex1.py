89c4f0c4a2677e325f760e6fbdedaafb5f6e87e5
Kris Monroe <krisdmonroe@gmail.com>
2020-06-04 22:34:04 -0700

    One down

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,35 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    # x = weights
+    # for i in range(len(weights)):
+    #     for ii in range(len(weights) -1):
+    #         if (x[i] + x[ii+1]) == limit:
+    #                 return (ii+1 , i)
+    # (key,value)
+    # (weights, index)
+        # print(i)
+    for i in range(len(weights)):
+        # print(i, end = " ")
+        # print(weights[i])
+        # print(i)
+        x = limit - weights[i]
+        # x is limit minus weight now were looking for that value in the cache
+        if x in cache:
+            if cache[x] < i:
+                return [i, cache[x]]
+            else:    
+                return [cache[x], i]
+            # print(cache)
+        # otherwise add it to the cache
+        else:
+            # weights[i] is value and i is index
+            cache[weights[i]] = i
+        
+        # print(limit - weights[i]) 
+    return None 
 
-    return None
+# if __name__ == "__main__":
+#     weights_3 = [4, 6, 10, 15, 16]
+#     print(get_indices_of_item_weights(weights_3, 5, 21))
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,27 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    for i in a:
+        new = abs(i)
+        if new not in cache:
+            cache[new] = 1
+        else:
+            cache[new] += 1
+            # print('this is cache', cache)
+            # print('this is greater', cache)
+        for key, value in cache.items():
+            if value == 2:
+                if key not in cache:
+                    print('this is print', key)
+                # print('this is print', key)
 
-    return result
+        # print('this is value', value)
+        # else:
+        #     new = abs(i)
+        #     print(new)
+    # print(a)
+    return key
 
 
 if __name__ == "__main__":
